805807e6e2fc79133f06b30af34a563eec989d0f
akshay-surjuse <108818182+akshay-surjuse@users.noreply.github.com>
2025-09-25 19:05:43 +0530

    SubjectRepository.java

--- src/main/java/com/digital/repository/SubjectRepository.java
+++ src/main/java/com/digital/repository/SubjectRepository.java
@@ -3,19 +3,6 @@ package com.digital.repository;
 import com.digital.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     boolean existsBySubjectCode(String subjectCode);
-
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
-    List<Subject> findAllBySchoolClass_ClassId(Long classId);
-
-    List<Subject> findAllByTeacher_Id(Long teacherId);
-
 }
