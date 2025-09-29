6521111f50a41f268ce5d16a7d8bed0c5e1de448
akshay-surjuse <108818182+akshay-surjuse@users.noreply.github.com>
2025-09-29 09:41:05 +0530

    Update SubjectRequest.java

--- src/main/java/com/digital/dto/SubjectRequest.java
+++ src/main/java/com/digital/dto/SubjectRequest.java
@@ -20,30 +20,4 @@ public class SubjectRequest {
     private String description;
     private BigDecimal maxMarks;
     private Boolean isActive;
-
-import com.digital.entity.SchoolClass;
-import com.digital.entity.Teacher;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.Year;
-
-@Setter
-@Getter
-@Builder
-public class SubjectRequest {
-
-    @NotBlank(message = "Subject name is required")
-    @Size(min = 4, max = 50, message = "Subject name must be between 4 to 100 characters")
-    private String subjectName; // → The name of the subject (e.g., Mathematics, Science, History)
-
-    private Teacher teacher; // Teacher_Reg_Id → Foreign Key (links the subject to a teacher who teaches it)
-
-    private SchoolClass schoolClass; // → The class/grade in which the subject is taught (e.g., Class 6, Class 10)
-
-    private Year year;
-
 }
